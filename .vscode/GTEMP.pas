529f5a1e9597ec59010f0807d1ac0071fa5e231a
Thiago Santana <thiagosan252@gmail.com>
2021-09-05 17:53:52 -0300

    first commit

---
+++ .vscode/GTEMP.pas
@@ -0,0 +1,31 @@
+//Fenotipos (A, B, AB, O)
+
+Program HelloWorld;
+var
+  a: integer;
+  b: integer;
+  ab: integer;
+  o: integer;
+begin
+  write('Digite o valor de A ');
+  readln(a); 
+
+  write('Digite o valor de B ');
+  readln(b); 
+
+  write('Digite o valor de AB ');
+  readln(ab); 
+
+  write('Digite o valor de O ');
+  readln(o); 
+
+  writeln('A => ', a);  
+  writeln('B => ', b);  
+  writeln('AB => ', ab);  
+  writeln('O => ', o);  
+end.
+
+
+
+
+
